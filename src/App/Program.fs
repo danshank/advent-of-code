77618ad352f4b097bb1f7d2f15b0fe1d2067b58b
Daniel Shank <dshank@suffolk.com>
2018-12-02 20:24:59 -0500

    Tried adding tests, gave up Instead wrote code to solve day 1

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,8 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Say
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let name = argv.[0]
+    hello name
     0 // return an integer exit code
